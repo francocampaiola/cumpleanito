a3189b8eda4c8daec035b31bb165dd33ea9bf6cc
Franco Campaiola <francocampaiola@hotmail.com>
2023-02-15 12:20:53 -0300

    add Text in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ import {
   InputLeftElement,
   InputRightElement,
   Text,
-  VStack 
-  } from "@chakra-ui/react";
+  VStack
+} from "@chakra-ui/react";
 import { MainLayout } from "layouts";
 
 export default function Home() {
@@ -16,25 +16,28 @@ export default function Home() {
     <>
       <MainLayout>
         <Container maxW={'5xl'} marginTop={'80px'}>
-          <VStack spacing={'50px'}>
+          <VStack spacing={'50px'} width={'100%'}>
             <VStack>
-              <Heading>Celebrar sin olvidar</Heading>
-              <Text fontSize={'sm'} maxW={'md'} textAlign={'center'}>Cumpleanito es la plataforma perfecta para recordar y celebrar los cumpleaños de tus amigos y seres queridos.</Text>
+              <Heading fontFamily={'sans-serif'}>Celebrar sin olvidar</Heading>
+              <Text fontFamily={'body'} fontSize={'sm'} maxW={'md'} textAlign={'center'}>Cumpleanito es la plataforma perfecta para recordar y celebrar los cumpleaños de tus amigos y seres queridos.</Text>
             </VStack>
-            <InputGroup size={'md'} maxW={'lg'} rounded={'full'}>
-              <InputLeftElement
-                pointerEvents='none'
-                color='gray.300'
-                children='cumpleanito.app/'
-                width={'150px'}
-              />
-              <Input placeholder='usuario' paddingLeft={'138px'} />
-              <InputRightElement width='7rem'>
-                <Button h='1.75rem' size='sm' colorScheme={'blue'}>
-                  Crear página
-                </Button>
-              </InputRightElement>
-            </InputGroup>
+            <VStack>
+              <InputGroup size={'md'} width={'450px'} rounded={'full'}>
+                <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.300'
+                  children='cumpleanito.app/'
+                  width={'150px'}
+                />
+                <Input placeholder='usuario' paddingLeft={'138px'} />
+                <InputRightElement width='7rem'>
+                  <Button h='1.75rem' size='sm' colorScheme={'blue'}>
+                    Crear página
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <Text fontSize={'xs'}>Es gratis y toma menos de un minuto.</Text>
+            </VStack>
           </VStack>
         </Container>
       </MainLayout>
