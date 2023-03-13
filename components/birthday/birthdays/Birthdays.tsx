0103bef1e34f3c4eb06a6c855941d6bbb2e6650c
Franco Campaiola <francocampaiola@hotmail.com>
2023-03-13 20:00:16 -0300

    fix add logic birthday

--- components/birthday/birthdays/Birthdays.tsx
+++ components/birthday/birthdays/Birthdays.tsx
@@ -7,10 +7,8 @@ export const Birthdays = () => {
   const currentYear = today.getFullYear();
   const sortedBirthdays = birthdays
     .map((bday) => {
-      const [day, month] = bday.birthday.split("/").map(Number);
+      const [day, month] = bday.birthday.split("/");
       const birthdate = new Date(currentYear, month - 1, day);
-      const ageInMs = today.getTime() - birthdate.getTime();
-      const age = Math.floor(ageInMs / (1000 * 60 * 60 * 24 * 365));
       if (birthdate < today) {
         birthdate.setFullYear(currentYear + 1);
       }
@@ -18,12 +16,23 @@ export const Birthdays = () => {
     })
     .sort((a, b) => a.birthdate.getTime() - b.birthdate.getTime());
 
+  // Filtra los cumpleaños que ocurren hoy y agregalos al principio de la lista
+  const todayBirthdays = sortedBirthdays.filter((bday) => {
+    const birthdate = bday.birthdate;
+    return (
+      birthdate.getDate() === today.getDate() &&
+      birthdate.getMonth() === today.getMonth()
+    );
+  });
+  const otherBirthdays = sortedBirthdays.filter((bday) => !todayBirthdays.includes(bday));
+  const sortedAndFilteredBirthdays = [...todayBirthdays, ...otherBirthdays];
+
   return (
     <Grid
       templateColumns={{ md: "repeat(4, 1fr)", base: "repeat(2, 1fr)" }}
       gap={5}
     >
-      {sortedBirthdays.map((bday) => (
+      {sortedAndFilteredBirthdays.map((bday) => (
         <BirthdayCard key={bday.id} {...bday} />
       ))}
     </Grid>
