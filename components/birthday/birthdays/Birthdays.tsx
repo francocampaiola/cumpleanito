0bc5f22adb6e982e59e3ac3028f015e1d7dfa61e
Franco Campaiola <francocampaiola@hotmail.com>
2023-03-13 19:57:18 -0300

    fix logic with dates birthday

--- components/birthday/birthdays/Birthdays.tsx
+++ components/birthday/birthdays/Birthdays.tsx
@@ -1,43 +1,33 @@
-import { Grid } from "@chakra-ui/react"
-import { BirthdayCard } from "../BirthdayCard"
-import { birthdays } from "constants/birthdays"
+import { Grid } from "@chakra-ui/react";
+import { BirthdayCard } from "../BirthdayCard";
+import birthdays from "../../../constants/birthdays";
 
 export const Birthdays = () => {
-  const birthdayList = birthdays.map((bday) => {
-    const parts = bday.birthday.split('/');
-    const birthdayDate = new Date(
-      new Date().getFullYear(),
-      parseInt(parts[1]) - 1,
-      parseInt(parts[0])
-    );
-    const today = new Date();
-    const nextBirthday = new Date(
-      today.getFullYear(),
-      birthdayDate.getMonth(),
-      birthdayDate.getDate()
-    );
-    if (nextBirthday < today) {
-      nextBirthday.setFullYear(today.getFullYear() + 1);
-    }
-    const timeDiff = nextBirthday.getTime() - today.getTime();
-    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
-    return {
-      ...bday,
-      birthdayDate,
-      daysDiff,
-    };
-  });
-
-  const sortedList = birthdayList.sort((a, b) => new Date(a.birthdayDate).getTime() - new Date(b.birthdayDate).getTime());
-
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const sortedBirthdays = birthdays
+    .map((bday) => {
+      const [day, month] = bday.birthday.split("/").map(Number);
+      const birthdate = new Date(currentYear, month - 1, day);
+      const ageInMs = today.getTime() - birthdate.getTime();
+      const age = Math.floor(ageInMs / (1000 * 60 * 60 * 24 * 365));
+      if (birthdate < today) {
+        birthdate.setFullYear(currentYear + 1);
+      }
+      return { ...bday, birthdate };
+    })
+    .sort((a, b) => a.birthdate.getTime() - b.birthdate.getTime());
 
   return (
-    <Grid templateColumns={{'md': "repeat(4, 1fr)", 'base': "repeat(2, 1fr)"}} gap={5}>
-      {sortedList.map((bday) => (
+    <Grid
+      templateColumns={{ md: "repeat(4, 1fr)", base: "repeat(2, 1fr)" }}
+      gap={5}
+    >
+      {sortedBirthdays.map((bday) => (
         <BirthdayCard key={bday.id} {...bday} />
       ))}
     </Grid>
   );
 };
 
+export default Birthdays;
